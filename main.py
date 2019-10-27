f03c0cf69c792a539bf8db1db3fe4057ecc878ae
Takashi Ando <dodo5522@gmail.com>
2019-10-28 00:32:11 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+'''
+this module is test for sonarcloud
+'''
+
+
+def main():
+    '''
+    this function is main
+    '''
+    print('hello world!')
+    sub()
+
+
+def sub():
+    '''
+    this function is sub
+    '''
+    print('hello world again!')
+
+
+def sub3():
+    '''
+    this function is sub
+    '''
+    print('hello world again!')
+
+
+if __name__ == '__main__':
+    main()
